8088c9fd0b475b87f6c93518126237d84b29030f
Daniel Alejandro Hernández Gálvez <109681970+DanielHernandezGalvez@users.noreply.github.com>
2023-01-26 22:02:44 -0600

    Update main.go

--- main.go
+++ main.go
@@ -3,35 +3,35 @@ package main
 import "fmt"
 
 //constantes
-// const Curso string = "Curso Go"
+const string = "Curso Go"
 
 func main() {
 
-	// var nombre string = "nombre"
-	// var edad int =25
+	var nombre string = "nombre"
+	var edad int =25
 
-	// nombre2 := "nombre"
-	// edad2 := 5
+	nombre2 := "nombre"
+	edad2 := 5
 
-	// var altura = 1.67
+	var altura = 1.67
 
-	// fmt.Println(nombre2, edad2, altura)
-	// fmt.Println(nombre2) 
-	// fmt.Println(edad2)
-	// fmt.Println(altura)
+	fmt.Println(nombre2, edad2, altura)
+	fmt.Println(nombre2) 
+	fmt.Println(edad2)
+	fmt.Println(altura)
 
-	// fmt.Println("nombre:", nombre2, "edad:", edad2, "altura", altura)
+	fmt.Println("nombre:", nombre2, "edad:", edad2, "altura", altura)
 
 	//variables multiples
-	// var nombre, apellido, pais string
-	// var nombre, apellido, pais = "daniel", "galvez", "mexico"
-	// nombre, apellido, pais := "daniel", "galvez", "mexico"
-	// edad, altura := 26, 1,69
+	var nombre, apellido, pais string
+	var nombre, apellido, pais = "daniel", "galvez", "mexico"
+	nombre, apellido, pais := "daniel", "galvez", "mexico"
+	edad, altura := 26, 1,69
 
-	// fmt.Println(nombre, apellido, pais, edad, alura)
+	fmt.Println(nombre, apellido, pais, edad, alura)
 
 	//constante se escribe dentro de la función
-	// fmt.Println(Curso)
+	fmt.Println(Curso)
 
 	//Operadores Relacionales
 	// <
