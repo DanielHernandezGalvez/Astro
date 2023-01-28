1d4eac530813533fd321322fae8c15dd319ba4ad
Daniel Alejandro Hernández Gálvez <109681970+DanielHernandezGalvez@users.noreply.github.com>
2023-01-28 12:07:17 -0600

    Update tipos.go

--- tipos.go
+++ tipos.go
@@ -7,10 +7,14 @@ func main() {
   var a rune = -100
   
   var b rune = "a"
-  
+	c := uint16(a) + b
 	var a bool = true
-
+	_ := 234 //asi le digo que no la usaré ero no la borraré
+	var _ string = "test" // igual que arriba
+	
+	var a string //el valor asignado es igual a 0
+	
 	fmt.Printf("Tipo: %T, value: %v", a, a)
-  
+
   
 }
