cacc6527813c61a12a3e4c68b3a26c6fd29910a6
Daniel Alejandro Hernández Gálvez <109681970+DanielHernandezGalvez@users.noreply.github.com>
2023-01-30 15:57:17 -0600

    Update main.go

--- bootcamp/condicionales/main.go
+++ bootcamp/condicionales/main.go
@@ -3,14 +3,14 @@ package main
 import "fmt"
 
 func main() {
-	/* 	emoji := "🐈"
+		emoji := "🐈"
 	   	if emoji == "🌵" {
 	   		fmt.Println("es un cactus")
 	   	} else if emoji == "😋" {
 	   		fmt.Println("es una carita")
 	   	} else {
 	   		fmt.Printf("el emoji es: %s\n", emoji)
-	   	} */
+	   	} 
 
 	if emoji := "😋"; emoji == "🌵" {
 		fmt.Println("es un cactus")
