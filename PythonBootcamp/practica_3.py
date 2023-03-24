b63faaa37ff2c910c3f635f8688b67b4e18faa6a
Daniel Alejandro Hernández Gálvez <109681970+DanielHernandezGalvez@users.noreply.github.com>
2023-03-24 15:35:46 -0600

    Update practica_3.py

--- PythonBootcamp/practica_3.py
+++ PythonBootcamp/practica_3.py
@@ -127,3 +127,46 @@ print((17834/34)>87*56)
 # Práctica Booleanos 3
 
 print(25**0.5==5)
+
+texto = input("ingresa un texto a elección: ")
+letras = []
+
+texto = texto.lower()
+
+letras.append(input("ingresa la primera letra: ").lower())
+letras.append(input("ingresa la segunda letra: ").lower())
+letras.append(input("ingresa la tercera letra: ").lower())
+
+print("\n")
+print("Cantidad de letras")
+cantidad_letras = texto.count(letras[0])
+cantidad_letras2 = texto.count(letras[1])
+cantidad_letras3 = texto.count(letras[2])
+
+print(f"Hemos encontrado la letra '{letras[0]}' repetida {cantidad_letras} veces")
+print(f"Hemos encontrado la letra '{letras[1]}' repetida {cantidad_letras2} veces")
+print(f"Hemos encontrado la letra '{letras[2]}' repetida {cantidad_letras3} veces")
+
+print("\n")
+print("Cantidad de palabras")
+
+palabras = texto.split()
+print(f"Hemos encntrado {len(palabras)} palabras en tu texto")
+
+print("\n")
+print("Primera y Última letra")
+letra_inicio = texto[0]
+letra_fin = texto[-1]
+print(f"la letra inicial es{letra_inicio} y la última es {letra_fin}")
+
+print("\n")
+print("Texto invertido")
+palabras.reverse()
+texto_invertido = " ".join(palabras)
+print(f"Si ordenamos el texto al revés, va a decir: '{texto_invertido}'")
+
+print("\n")
+print("Buscando la palabra Python")
+buscar_python = "python" in texto
+dic = {True: "si", False: "no"}
+print(f"la palabra 'Python' {dic[buscar_python]} se encuentra en el texto")
