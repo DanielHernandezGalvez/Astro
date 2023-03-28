178d874028d9d1356b680442edbf1d46351ddd2d
Daniel Alejandro Hernández Gálvez <109681970+DanielHernandezGalvez@users.noreply.github.com>
2023-03-28 16:43:46 -0600

    Update main.py

--- bootcamp/constantes/main.py
+++ bootcamp/constantes/main.py
@@ -1,9 +1,18 @@
-package main
+from bokeh.plotting import figure, output_file, show
 
-import "fmt"
 
-func main() {
-	const pi = 3.14
+if __name__ == '__main__':
+    output_file('simple_graph.html')
+    graph = figure()
 
-	fmt.Println(pi)
-}
+    print('**** GRAFICADOR ****')
+    total_values = int(input('➡ Cuántos valores quieres graficar: '))
+    x_values = list(range(total_values))
+    y_values = []
+
+    for i in x_values:
+        value = int(input(f'Valor y para {i}'))
+        y_values.append(value)
+
+    graph.line(x_values, y_values, line_width=2)
+    show(graph)
