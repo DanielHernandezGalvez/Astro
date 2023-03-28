5327ca4843777a7a635eeeb9bacf2611e2b1b77b
Daniel Alejandro Hernández Gálvez <109681970+DanielHernandezGalvez@users.noreply.github.com>
2023-03-28 16:09:43 -0600

    Update main.py

--- bootcamp/comentarios/main.py
+++ bootcamp/comentarios/main.py
@@ -1,15 +1,27 @@
-// package main es el paquete principal
-// que me permite ejecutar mi aplicación
-// de EDteam
-package main
+import random
 
-// Test es una variable de prueba
-var Test = 123
 
-// Pi es una constante que vale 3.14
-const Pi = 3.14
+def bubble_sort(elements_list):
+    n = len(elements_list)
 
-/* func main() {
+    # Itera cada elemento de la lista
+    for i in range(n):
+        # Por cada elemento recorre la lista hasta el final y luego se va  reduciendo porque los elementos del final ya
+        # están ordenados
+        # recorrer desde 0 hasta la longitud - lo que ya se recorrió - 1 porque se trabaja con un len
+        for j in range(0, n - i - 1):
+            if elements_list[j] > elements_list[j + 1]:
+                elements_list[j], elements_list[j + 1] = elements_list[j + 1], elements_list[j]
 
-}
-*/
+    return elements_list
+
+
+if __name__ == '__main__':
+    print('**** ORDENAMIENTO DE BURBUJA ****')
+    list_length = int(input('➡ Ingresa el tamaño de la lista: '))
+
+    numbers_list = [random.randint(0, 100) for i in range(list_length)]
+    print(numbers_list)
+
+    ordered_list = bubble_sort(numbers_list)
+    print(ordered_list)
