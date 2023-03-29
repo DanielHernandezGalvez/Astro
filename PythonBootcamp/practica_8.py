90e104c664bf8159b7a39205d0fbf080ff322e87
Daniel Alejandro Hernández Gálvez <109681970+DanielHernandezGalvez@users.noreply.github.com>
2023-03-29 11:29:41 -0600

    Update practica_8.py

--- PythonBootcamp/practica_8.py
+++ PythonBootcamp/practica_8.py
@@ -73,3 +73,80 @@ def mensaje():
     yield x
  
 perder_vida = mensaje()
+
+# Generador de turnos
+# archivo numeros.py
+
+
+def numeros_perfumeria():
+    for n in range(1, 10000):
+        yield f"P - {n}"
+
+
+def numeros_farmacia():
+    for n in range(1, 10000):
+        yield f"F - {n}"
+
+
+
+def numeros_cosmetica():
+    for n in range(1, 10000):
+        yield f"C - {n}"
+
+
+p = numeros_perfumeria()
+f = numeros_farmacia()
+c = numeros_cosmetica()
+
+
+def decorador(rubro):
+
+    print("\n" + "*" * 23)
+    print("Su numero es: ")
+    if rubro == "P":
+        print(next(p))
+    elif rubro == "F":
+        print(next(f))
+    else:
+        print(next(c))
+    print("Aguarde y será atendido")
+    print('*' * 23 + "\n")
+
+
+# principal.py
+
+import numeros
+
+def preguntar():
+
+    print("Bienvenido a farmacia")
+
+    while True:
+        print("[P] - perfumeria\n[F] - Farmacia\n[C] - cosmetica")
+        try:
+            mi_rubro = input("Elija rubro: ").upper()
+            ["P", "F", "C"].index(mi_rubro)
+        except ValueError:
+            print("Esa no es una opcion valida")
+        else:
+            break
+    
+    numeros.decorador(mi_rubro)
+
+
+def inicio():
+
+    while True:
+        preguntar()
+        try:
+            otro_Turno = input("Quieres sacar otro turno?").upper()
+            ["S", "N"].index(otro_Turno)
+        except ValueError:
+            print("esa no es una opcion valida")
+        else:
+            if otro_Turno == "N":
+                print("gracias por su visita")
+                break
+
+inicio()
+
