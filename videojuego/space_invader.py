0888239f30733fb53c0823c4f75743a19028f504
Daniel Alejandro Hernández Gálvez <109681970+DanielHernandezGalvez@users.noreply.github.com>
2023-04-01 11:16:03 -0600

    Update space_invader.py

--- videojuego/space_invader.py
+++ videojuego/space_invader.py
@@ -1,5 +1,6 @@
 import pygame
 import random
+import math
 
 # Inicializar pygame
 pygame.init()
@@ -23,11 +24,30 @@ jugador_y = 500
 jugador_x_cambio = 0
 
 # enemigo
-img_enemigo = pygame.image.load("imagen del enemigo")
-enemigo_x = random.randint(0, 736)
-enemigo_y = random.randint(50, 200)
-enemigo_x_cambio = 1
-enemigo_y_cambio = 50
+img_enemigo = []
+enemigo_x = []
+enemigo_y = []
+enemigo_x_cambio = []
+enemigo_y_cambio = []
+cantidad_enemigos = 8
+
+for e in range(cantidad_enemigos):
+    img_enemigo.append(pygame.image.load("imagen del enemigo"))
+    enemigo_x.append(random.randint(0, 736))
+    enemigo_y.append(random.randint(50, 200))
+    enemigo_x_cambio.append(0.5)
+    enemigo_y_cambio.append(50)
+
+# bala
+img_bala = pygame.image.load("imagen de la bala")
+bala_x = 0
+bala_y = 500
+bala_x_cambio = 0
+bala_y_cambio = 1
+bala_visible = False
+
+# puntaje
+puntaje = 0
 
 
 # funcion jugador
@@ -35,8 +55,23 @@ def jugador(x, y):
     pantalla.blit(img_jugador, (x, y))
 
 # funcion enemigo
-def enemigo(x, y):
-    pantalla.blit(img_enemigo, (x, y))
+def enemigo(x, y, ene):
+    pantalla.blit(img_enemigo[ene], (x, y))
+
+# funcion disparar 
+def disparar_bala(x, y):
+    global bala_visible
+    bala_visible = True
+    pantalla.blit(img_bala, (x + 16, y + 10))
+
+
+# Funcion para detectar colisiones
+def hay_colision(x_1, y_1, x_2, y_2):
+    distancia = math.sqrt(math.pow(x_1 - x_2, 2) + math.pow(y_2 - y_1, 2))
+    if distancia < 27:
+        return True
+    else:
+        return False
 
 
 
@@ -61,6 +96,10 @@ while se_ejecuta:
                 jugador_x_cambio -= 1
             if evento.key == pygame.K_RIGHT:
                 jugador_x_cambio = 1
+            if evento.key == pygame.K_SPACE:
+                if not bala_visible == False:
+                    bala_x = jugador_x
+                    disparar_bala(bala_x, bala_y)
 
         # evento soltar teclas
         if evento.type == pygame.KEYUP:
@@ -77,18 +116,43 @@ while se_ejecuta:
         jugador_x = 736
 
      # ubicacion del enemigo
-    enemigo_x += enemigo_x_cambio
+    for e in range(cantidad_enemigos):
+        enemigo_x[e] += enemigo_x_cambio[e]
 
     # mantener dentro de los bordes al enemigo
-    if enemigo_x <= 0:
-        enemigo_x = 1
-        enemigo_y += enemigo_y_cambio
-    elif enemigo_x <= 736:
-        enemigo_x_cambio = -1
-        enemigo_y += enemigo_y_cambio
+        if enemigo_x[e] <= 0:
+            enemigo_x[e] = 1
+            enemigo_y[e] += enemigo_y_cambio[e]
+        elif enemigo_x[e] <= 736:
+            enemigo_x_cambio[e] = -1
+            enemigo_y[e] += enemigo_y_cambio[e]
+
+          # colision
+        colicion = hay_colision(enemigo_x[e], enemigo_y[e], bala_x, bala_y)
+        if colicion:
+            bala_y = 500
+            bala_visible = False
+            puntaje += 1
+            print(puntaje)
+            enemigo_x[e] = random.randint(0, 736)
+            enemigo_y[e] = random.randint(50, 200)
+        
+        enemigo(enemigo_x[e], enemigo_y[e], e)
+
+
+    # movimiento bala
+    if bala_y <= -64:
+        bala_y == 500
+        bala_visible = False
+
+    if bala_visible:
+        disparar_bala(bala_x, bala_y)
+        bala_y -= bala_y_cambio
+
+  
+
 
     jugador(jugador_x, jugador_y)
-    enemigo(enemigo_x, enemigo_y)
 
     # actualizar
     pygame.display.update()
