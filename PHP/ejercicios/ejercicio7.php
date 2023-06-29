ed764d8b44692413087e0b6c3a448408309f17a8
Daniel Alejandro Hernández Gálvez <109681970+DanielHernandezGalvez@users.noreply.github.com>
2023-06-28 18:31:32 -0600

    ejercicios 7 y 8

---
+++ PHP/ejercicios/ejercicio7.php
@@ -0,0 +1,8 @@
+<?php
+  /*
+    Crear un array llamado meses y que almacene el nombre de los 
+    doce meses del año.
+    Recorrelo con un FOR para mostrar por pantalla los 12 meses.
+    */
+
+?>
