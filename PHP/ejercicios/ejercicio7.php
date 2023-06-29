0095f074091c8c911df17de47508715946130908
Daniel Alejandro Hernández Gálvez <109681970+DanielHernandezGalvez@users.noreply.github.com>
2023-06-28 18:41:35 -0600

    ejercicio8

--- PHP/ejercicios/ejercicio7.php
+++ PHP/ejercicios/ejercicio7.php
@@ -2,7 +2,25 @@
   /*
     Crear un array llamado meses y que almacene el nombre de los 
     doce meses del año.
-    Recorrelo con un FOR para mostrar por pantalla los 12 meses.
+    Recorrelo con un ForEach para mostrar por pantalla los 12 meses.
     */
+    $meses = array(
+        "Enero", 
+        "Febrero", 
+        "Marzo",
+        "Abril",
+        "Mayo",
+        "Junio",
+        "Julio",
+        "Agosto",
+        "Septiembre",
+        "Octubre",
+        "Noviembre",
+        "Diciembre"
+    );
+
+    foreach($meses as $mes) {
+        echo $mes."<br/>";
+    }
 
 ?>
