1ac2fc137f25d61000d0dd1f58081a893894ce1f
Daniel Alejandro Hernández Gálvez <109681970+DanielHernandezGalvez@users.noreply.github.com>
2023-06-28 18:54:18 -0600

    facilito

--- PHP/tutorial/IntroduccionPHP/17_class.php
+++ PHP/tutorial/IntroduccionPHP/17_class.php
@@ -3,21 +3,21 @@
 ?>
 
 <?php
-  class Cliente {
-    public function __construct($nombre)
-    {
-      $this->nombre = $nombre;
-    }
+  // class Cliente {
+  //   public function __construct($nombre)
+  //   {
+  //     $this->nombre = $nombre;
+  //   }
 
-    public function mostrar_nombre(){
-      echo "Se shama ".$this->nombre;
-    }
-  }
-  $cliente = new Cliente("oscar");
-  $cliente->mostrar_nombre();
+  //   public function mostrar_nombre(){
+  //     echo "Se shama ".$this->nombre;
+  //   }
+  // }
+  // $cliente = new Cliente("oscar");
+  // $cliente->mostrar_nombre();
 
-  $cliente_2 = new Cliente("abraham");
-  $cliente_2->mostrar_nombre();
+  // $cliente_2 = new Cliente("abraham");
+  // $cliente_2->mostrar_nombre();
 
   // echo "<pre>";
   // var_dump($cliente);
