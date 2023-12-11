348dda4dc0c7f62e3479f52082a82e12ff8caefa
Daniel Alejandro Hernández Gálvez <hernandezgalvezalejandro@gmail.com>
2023-12-10 23:15:39 -0600

    sin web

--- rest_framework/blog/posts/api/serializers.py
+++
@@ -1,8 +0,0 @@
-from rest_framework.serializers import ModelSerializer
-from posts.models import Post
-
-class PostSerializer(ModelSerializer):
-    class Meta:
-        model = Post
-        fields = ['title', 'description', 'create_at'] # devuelve lo que se solicitó
-        # fields = '__all__' devuelve todo
