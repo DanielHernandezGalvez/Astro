0722767f31a17ec737726e42e2d788fe3804119c
unknown <hernandezgalvezalejandro@gmail.com>
2023-12-31 18:29:51 -0600

    colores del texto

--- admin/src/components/SkillCard.jsx
+++ admin/src/components/SkillCard.jsx
@@ -3,7 +3,7 @@ import React from 'react';
 const SkillCard = ({ label }) => {
   return (
     <div
-      class="border rounded-full  opacity-80 hover:bg-blue-500 
+      class="border rounded-full text-gray-800  opacity-80 hover:bg-blue-500 
       cursor-pointer hover:text-white transition hover:scale-105
       py-1 px-2 lg:py-2 lg:px-4 text-xs lg:text-base"
     >
